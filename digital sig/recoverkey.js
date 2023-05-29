73142720ff2b938ac26a9b28b9af4d5ae2e4e0ec
NeoZ666 <neoz.blockchain@gmail.com>
2023-05-29 22:08:21 +0530

    reocvery key

---
+++ digital sig/recoverkey.js
@@ -0,0 +1,9 @@
+keccaconst secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    const msgHash = await hashMessage(message);
+    return secp.recoverPublicKey(msgHash, signature, recoveryBit, isCompressed = false)
+}
+
+module.exports = recoverKey;
