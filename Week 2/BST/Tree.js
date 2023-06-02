6fd9e8fbcfac76eba1feab8244ad55f59bb2dd8f
NeoZ666 <neoz.blockchain@gmail.com>
2023-06-02 22:12:31 +0530

    trees update

--- Week 2/BST/Tree.js
+++ Week 2/BST/Tree.js
@@ -1,7 +1,56 @@
 class Tree {
     constructor() {
-        this.root = null;
+      this.root = null;
     }
-}
-
-module.exports = Tree;
+  
+    addNode(child) {
+      if (!this.root) {
+        this.root = child;
+      } else {
+        this.addChildToParent(this.root, child);
+      }
+    }
+  
+    addChildToParent(parent, child) {
+      if (child.data < parent.data) {
+        if (parent.left) {
+          this.addChildToParent(parent.left, child);
+        } else {
+          parent.left = child;
+        }
+      } else if (child.data > parent.data) {
+        if (parent.right) {
+          this.addChildToParent(parent.right, child);
+        } else {
+          parent.right = child;
+        }
+      }
+    }
+  
+    hasNode(number) {
+      if (!this.root) {
+        return false;
+      }
+      return this.searchNode(number, this.root);
+    }
+  
+    searchNode(number, node) {
+      if(node) {
+        if(number < node.data) {
+          return this.searchNode(number, node.left);
+        }
+        if(number > node.data) {
+          return this.searchNode(number, node.right);
+        }
+        else {
+          return true;
+        }
+      }
+      else if(!node) {
+        return false;
+      }
+    }
+  }
+  
+  
+  module.exports = Tree;
