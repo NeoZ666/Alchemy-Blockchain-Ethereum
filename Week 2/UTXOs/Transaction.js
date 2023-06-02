cc49a97916a11a78557b5c29729f5f38b8155d47
NeoZ666 <neoz.blockchain@gmail.com>
2023-06-02 19:56:47 +0530

    uodate trans

--- Week 2/UTXOs/Transaction.js
+++ Week 2/UTXOs/Transaction.js
@@ -7,6 +7,18 @@ class Transaction {
         if(this.input.some((x)=>(x.spent))) {
             throw error("Can't double spend");
         }
+        let total = 0;
+        this.input.forEach((x) => {
+            total += x.amount;
+        })
+        const inputTotal = total;
+        total = 0;
+        this.output.forEach((x)=>{
+            total += x.amount;
+        })
+        if(inputTotal < total) {
+            throw new error("Insuffficient balance")
+        }
     }
 }
 
