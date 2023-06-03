bb9df11579e593b3d1e592c59382f5c3ce8e88b6
NeoZ666 <neoz.blockchain@gmail.com>
2023-06-03 19:27:46 +0530

    update trie

--- Week 2/Trie-PMTs-MT/Trie.js
+++ Week 2/Trie-PMTs-MT/Trie.js
@@ -4,6 +4,28 @@ class Trie {
     constructor() {
         this.root = new TrieNode(null);
     }
+
+    insert(string) {
+        let node = this.root;
+        for(let i=0;i<string.length;i++) {
+            if(!node.children[string[i]])
+                node.children[string[i]] = new TrieNode(string[i]);
+            node = node.children[string[i]];
+            if(i==string.length-1) 
+                node.isWord = true;
+        }
+    }
+
+    contains(word) {
+        let node = this.root;
+        for(let i=0;i<word.length;i++) {
+            if(!node.children[word[i]])
+                return false;
+            else
+                node = node.children[word[i]];
+        }
+        return node.isWord;
+    }
 }
 
 module.exports = Trie;
